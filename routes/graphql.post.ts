a0d6f05bded7b678c68709d8d6df21a7e557c7ea
sgohlke <gohlke.stefan@yahoo.de>
2025-02-04 13:47:56 +0100

    Use graphql-testing

--- routes/graphql.post.ts
+++ routes/graphql.post.ts
@@ -7,74 +7,10 @@ import {
   GraphQLServer, 
   JsonLogger 
 } from '@dreamit/graphql-server'
-
-interface User {
-  userId: string
-  userName: string
-}
-
-interface LogoutResult {
-  result: string
-}
-
-const userOne: User = {userId: '1', userName:'UserOne'}
-const userTwo: User = {userId: '2', userName:'UserTwo'}
-
-const userSchema = buildSchema(`
-  schema {
-    query: Query
-    mutation: Mutation
-  }
-  
-  type Query {
-    returnError: User 
-    users: [User]
-    user(id: String!): User
-  }
-  
-  type Mutation {
-    login(userName: String, password: String): LoginData
-    logout: LogoutResult
-  }
-  
-  type User {
-    userId: String
-    userName: String
-  }
-  
-  type LoginData {
-    jwt: String
-  }
-  
-  type LogoutResult {
-    result: String
-  }
-`)
-
-const userSchemaResolvers= {
-  returnError(): User {
-      throw new GraphQLError('Something went wrong!', {})
-  },
-  users(): User[] {
-      return [userOne, userTwo]
-  },
-  user(input: { id: string }): User {
-      switch (input.id) {
-      case '1': {
-          return userOne
-      }
-      case '2': {
-          return userTwo
-      }
-      default: {
-          throw new GraphQLError(`User for userid=${input.id} was not found`, {})
-      }
-      }
-  },
-  logout(): LogoutResult {
-      return {result: 'Goodbye!'}
-  }
-}
+import { 
+  userSchema,
+  userSchemaResolvers 
+} from '@dreamit/graphql-testing'
 
 const graphqlServer = new GraphQLServer(
   {
